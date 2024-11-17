8e4fef98711995c1e80afa007cfaca94c21e01ae
TPatel82003 <tirthpatel31082003@gmail.com>
2024-11-17 16:28:37 +0000

    Restructuring

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     long_description_content_type="text/markdown",
     author="Your Name",
     author_email="your.email@example.com",
-    url="https://github.com/TPatel82003/LeetCodeProblemFetcher",
+    url="https://github.com/TPatel82003/leetcode_problem_fetcher",
     license="MIT",
     packages=find_packages(),
     classifiers=[
